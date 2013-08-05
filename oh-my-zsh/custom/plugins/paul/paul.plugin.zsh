c8b64039b1adb01e4b241c715ac893fe2c62e645
paulthegeek <pj2454@gmail.com>
2013-08-05 09:43:33 -0600

    Initial dotfiles commit. I'm a big boy now!

---
+++ oh-my-zsh/custom/plugins/paul/paul.plugin.zsh
@@ -0,0 +1,21 @@
+unsetopt correct_all
+
+# Sublime Editor
+alias sub='sublime -n .'
+alias subd='sublime -n'
+
+# Git
+alias g='git'
+alias gs='git status'
+
+#Bundler
+alias b='bundle'
+alias be='bundle exec'
+alias bi='bundle install'
+
+#Rails
+alias rs='./script/rails s'
+alias rc='./script/rails c'
+
+#ZSH
+alias reload='. ~/.zshrc'
